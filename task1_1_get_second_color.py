3fd6f0d1796ea1f032a936a11a1e8fbe1f31aeed
unknown <shamsiyevalibek90@gmail.com>
2025-05-13 21:14:13 +0300

    samir1

--- task1_1_get_second_color.py
+++ task1_1_get_second_color.py
@@ -6,4 +6,13 @@ def get_second_color(colors: tuple) -> str:
     Returns:
         str: The second color in the tuple
     """
-    return
+    if not isinstance(colors, tuple):
+        raise TypeError("Input must be a tuple.")
+    if len(colors) != 3:
+        raise ValueError("Tuple must contain exactly three colors.")
+    return colors[1]
+colors = ("qizil", "yashil", "ko'k")
+print(get_second_color(colors))  # Natija: yashil
+
+
+
