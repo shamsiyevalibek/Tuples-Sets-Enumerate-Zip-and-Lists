78c9420834d95f54fc20b5ebfe6a58558da37486
unknown <shamsiyevalibek90@gmail.com>
2025-05-13 21:42:01 +0300

    samir5

--- task1_5_find_oldest.py
+++ task1_5_find_oldest.py
@@ -6,4 +6,8 @@ def find_oldest(people: list) -> tuple:
     Returns:
         tuple: A tuple containing (name, age) of the oldest person
     """
-    return
+    if not people:
+        raise ValueError("The list of people cannot be empty.")
+    return max(people, key=lambda person: person[1])
+people = [("Ali", 25), ("Vali", 30), ("Gulnora", 28)]
+print(find_oldest(people))
